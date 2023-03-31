de3ed958eaec72fce018b49041ea0ddd97acf3f3
Scarlet Rosa <likedyourbody@gmail.com>
2023-03-31 22:13:04 +0200

    who am i

---
+++ index.js
@@ -0,0 +1,14 @@
+const whoAmI = {
+    name : "Scarlet",
+    age : 25,
+    isDeveloper : true,
+    birthday : new Date(1997, 6, 25),
+    favBook : {
+        title: "O peso do pássaro morto",
+        author: "Aline Bei",
+        date: new Date(2017),
+        url: "https://editoranos.com.pt/?product=o-peso-do-passaro-morto"
+    }
+}
+
+console.log(whoAmI);
